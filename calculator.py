b9482f3fb644e69f477c1a4c00f0c11aebf83950
Amey <tendulkaramey.git@gmail.com>
2023-03-17 11:48:48 +0530

    modified logic and added test file

--- calculator.py
+++ calculator.py
@@ -15,36 +15,39 @@ def squareroot(number):
 def naturallog(number):
     return math.log(number)
 
+def startcalculator():
+    print('Welcome User')
+    print('Select the option')
 
-print('Welcome User')
-print('Select the option')
 
 
+    while True:
+        print('-----------------********----------------')
+        print('1.Square root\n2.factorial\n3.log\n4.power\n5.exit')
+        inputnumber = int(input())
+        if inputnumber == 5:
+            break
+        if inputnumber == 1:
+            print('enter number')
+            no = float(input())
+            print('square root of given no is {0}'.format(squareroot(no)))
+        elif inputnumber == 2:
+            print('enter the no to find factorial')
+            #check if no else error
+            i = int(input())
+            print('factorial for given no is {0}'.format(factorial(i)))
+        elif inputnumber == 3:
+            print('enter the no to find log')
+            no = float(input())
+            print('log of given no is {0}'.format(naturallog(no)))
+        elif inputnumber == 4:
+            print('enter base')
+            base = float(input())
+            print('enter exponent')
+            exponent = float(input())
+            print('base to the power exponent with given values is {0}'.format(power(base,exponent)))
+        else:
+            print('please select proper option')
 
-while True:
-    print('-----------------********----------------')
-    print('1.Square root\n2.factorial\n3.log\n4.power\n5.exit')
-    inputnumber = int(input())
-    if inputnumber == 5:
-        break
-    if inputnumber == 1:
-        print('enter number')
-        no = float(input())
-        print('square root of given no is {0}'.format(squareroot(no)))
-    elif inputnumber == 2:
-        print('enter the no to find factorial')
-        #check if no else error
-        i = int(input())
-        print('factorial for given no is {0}'.format(factorial(i)))
-    elif inputnumber == 3:
-        print('enter the no to find log')
-        no = float(input())
-        print('log of given no is {0}'.format(naturallog(no)))
-    elif inputnumber == 4:
-        print('enter base')
-        base = float(input())
-        print('enter exponent')
-        exponent = float(input())
-        print('base to the power exponent with given values is {0}'.format(power(base,exponent)))
-    else:
-        print('please select proper option')
+if __name__ == "__main__":
+    startcalculator()
--- test.py
+++ test.py
@@ -1,1 +1,40 @@
-print('this is test file')
+from calculator import *
+import unittest
+
+
+class testfactorial(unittest.TestCase):
+
+    def test_factorialpositive(self):
+        self.assertEqual(factorial(3),6,'should be 6')
+
+    def test_factorialnegative(self):
+        self.assertNotEqual(factorial(5),6,'should be 120')
+
+class testsquareroot(unittest.TestCase):
+
+    def test_squareroottestpositive(self):
+        self.assertEqual(squareroot(4),2,'should be 2')
+
+    def test_squarerootnegative(self):
+        self.assertNotEqual(squareroot(64),6,'should be 8')
+
+class testpower(unittest.TestCase):
+
+    def test_powerpositive(self):
+        self.assertEqual(power(2,3),8,'should be 8')
+
+    def test_powernegative(self):
+        self.assertNotEqual(power(2,2),6,'should be 4')
+
+class testlog(unittest.TestCase):
+
+    def test_logpositive(self):
+        self.assertEqual(naturallog(1),0,'should be 0')
+
+    def test_lognegative(self):
+        self.assertNotEqual(naturallog(25),2,'should be 3.2188758248682006')
+
+
+if __name__ == "__main__":
+    unittest.main()
+    print("Everything passed")
